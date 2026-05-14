eaae1556526ce8d74b5ed6467a410904a28bad03
Earner1-max <ga5695136@gmail.com>
2026-05-14 06:07:04 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+export const BOT_TOKEN = "8289206107:AAHiVEDBg2bnyyBjsIKP53gJO_hAE1vQftc";
+
+export const ADMIN_ID = 5574348933;
+
+export const OXAPAY_API_KEY = "F8DZEL-K5S3KF-FIK1NC-XYHL66";
+
+export const OXAPAY_BASE_URL = "https://api.oxapay.com";
+
+export const DATABASE_FILE = "./database.db";
+
+export const DEFAULT_REFER_AMOUNT = 100;
+
+export const DEFAULT_MINE_AMOUNT = 1000;
+
+export const DEFAULT_MIN_WITHDRAWAL = 5000;
+
+export const DEFAULT_MINE_COOLDOWN = 86400;
